aeabb15bbc6c2fae0d2617d855166930e552de70
abishekvp <abishek.vp13@gmail.com>
2024-03-06 13:00:47 +0530

    update 2

--- amrish/views.py
+++ amrish/views.py
@@ -9,15 +9,32 @@ import datetime
 def is_amrish(user):
     return user.groups.filter(name='amrish').exists()
 
-@user_passes_test(is_amrish)
 def home(request):
-    last_course={}
-    t=0
-    for i in db_courses.find():
-        if t<i["last_update"]:
-            t=i["last_update"]
-            last_course={"course_title":i["course_title"],"course_image":i["course_image"],"course_rating":i["course_rating"],"course_category":i["course_category"],"course_contains":i["course_contains"],"course_enrolled":i["course_enrolled"],"course_author":i["course_author"]}
-    return render(request, 'amrish/home.html',{'last_course':last_course})
+    if request.user.is_authenticated:
+        if request.user.groups.filter(name='amrish').exists():return redirect ("amrish-home")
+        elif request.user.groups.filter(name='buddy').exists():return redirect ("buddy-home")
+    def get_last_course():
+        last_course={}
+        t=0
+        for i in db_courses.find():
+            if t<i["last_update"]:
+                t=i["last_update"]
+                last_course={"course_title":i["course_title"],"course_image":i["course_image"],"course_rating":i["course_rating"],"course_category":i["course_category"],"course_contains":i["course_contains"],"course_enrolled":i["course_enrolled"],"course_author":i["course_author"]}
+        return last_course
+    
+    def get_popular_course():
+        popular_course={}
+        t=0
+        for i in db_courses.find():
+            if t<i["course_enrolled"]:
+                t=i["course_enrolled"]
+                popular_course={"course_title":i["course_title"],"course_image":i["course_image"],"course_rating":i["course_rating"],"course_category":i["course_category"],"course_contains":i["course_contains"],"course_enrolled":i["course_enrolled"],"course_author":i["course_author"]}
+        return popular_course
+    
+    popular_course = get_popular_course()
+    last_course = get_last_course()
+    print(popular_course,"\n\n",last_course)
+    return render(request, 'amrish/home.html',{"last_course":last_course,"popular_course":popular_course})
 
 @user_passes_test(is_amrish)
 def courses(request):
